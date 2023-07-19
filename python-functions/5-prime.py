9e8a49474913290a4451b58e2afd6a28fe84d00f
Hadizadefemi <adefemihadizi202@gmail.com>
2023-07-19 02:57:11 -0700

    task 5 done

---
+++ python-functions/5-prime.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def is_prime(number):
+    if number < 2:
+        return False
+    for divisor in range(2, int(number ** 0.5) + 1):
+        if number % divisor == 0:
+            return False
+    else:
+        return True
