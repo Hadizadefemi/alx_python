235946a7f4e8b70eceab78d41d0016533e186b67
Hadizadefemi <adefemihadizi202@gmail.com>
2023-07-26 15:46:50 -0700

    task corrected

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if len(matrix) == 0:
-        print()
+    if len(matrix) == 1:
+        if len(matrix[0]) == 0:
+            print()
     for i in matrix:
         for j in range(len(i)):
             if j == len(i) - 1:
