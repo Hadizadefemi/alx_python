3fd1026a6c1be2da482e691b21537ca78a8c508b
Hadiz <adefemihadizi2020@gmail.com>
2023-07-26 23:13:42 +0100

    task 1 done

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,19 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if len(matrix) == 1:
+        print()
     for i in matrix:
         for j in range(len(i)):
             if j == len(i) - 1:
                 print("{}".format(i[j]))
             else:
                 print("{} ".format(i[j]), end="")
-        
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]        
-
-print_matrix_integer(matrix)
-print("--")
-print_matrix_integer()
