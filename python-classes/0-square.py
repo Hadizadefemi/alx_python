ee0141170278162fe21380f2e8ab0c85fe9bc662
Hadiz <adefemihadizi2020@gmail.com>
2023-08-03 11:38:04 +0100

    task 0 done

---
+++ python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Defines a class Square."""
+
+class Square:
+    """Represents an instance as a square.
+    
+    Private instance attribute: size
+    """
+    
+    def __init__(self, size):
+        """Initialize square object with size"""
+        self.__size = size
