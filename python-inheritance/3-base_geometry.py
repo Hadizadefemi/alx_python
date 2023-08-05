4fd9f87f891a17f807d264b1f89d210953b9ffeb
Hadiz <adefemihadizi2020@gmail.com>
2023-08-05 12:42:50 +0100

    task3 done

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 """BaseGeometry: An empty class"""
 
-
-class BaseGeometry:
-    """An empty class"""
-    pass
+class RemoveInitSubClassMeta(type):
+    """overrides the dir attribute"""
+    def __dir__(self):
+        return [attr for attr in super().__dir__() if attr != '__init_subclass__']
+    
+class BaseGeometry(metaclass=RemoveInitSubClassMeta):
+    """removes __init_subclass from its attribute"""
+    def __dir__(self):
+        return [attr for attr in super().__dir__() if attr != '__init_subclass__']
