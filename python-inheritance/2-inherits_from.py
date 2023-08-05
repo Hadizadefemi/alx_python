695650aa7ba150bf7553a362355f517073fa0986
Hadiz <adefemihadizi2020@gmail.com>
2023-08-05 10:38:19 +0100

    corrected

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -3,14 +3,15 @@
 of a class that inherits directly or indirectly
 """
 
+
 def inherits_from(obj, a_class):
     """checks if an object is an instance of a 
     class that inherits directly or indirectly
-    
+
     Args:
         obj: object/instance of a class
         a_class: the specified class
-        
+
     Returns:
         bool: True if object is an instance of the class, False otherwise
     """
