6db8e5a1084bb3654a16a029155c9ebd32aad9a0
Hadiz <adefemihadizi2020@gmail.com>
2023-08-10 21:40:35 +0100

    corrected

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,30 +1,7 @@
 #!/usr/bin/env python3
 """BaseGeometry: A base class for any shape"""
+BaseGeometry = __import__('5-base_geometry').BaseGeometry
 
-
-class RemoveInitSubClassMeta(type):
-    """overrides the dir attribute"""
-    def __dir__(self):
-        """removes __init_subclass from its attribute"""
-        return [attr for attr in super().__dir__() if attr != '__init_subclass__']
-    
-class BaseGeometry(metaclass=RemoveInitSubClassMeta):
-    """Base class for any shape"""
-    def __dir__(self):
-        """removes __init_subclass from its attribute"""
-        return [attr for attr in super().__dir__() if attr != '__init_subclass__']
-    
-    def area(self):
-        """Not implemented"""
-        raise Exception('area() is not implemented')
-    
-    def integer_validator(self, name, value):
-        """Checks if value is a positive integer"""
-        if type(value) != int:
-            raise TypeError('{} must be an integer'.format(name))
-        elif value <= 0:
-            raise ValueError('{} must be greater than 0'.format(name))
-        
 
 class Rectangle(BaseGeometry):
     """Represents object as a rectangle"""
@@ -35,3 +12,5 @@ class Rectangle(BaseGeometry):
         super().integer_validator("height", height)
         self.__width = width
         self.__height = height
+        
+print(issubclass(Rectangle, BaseGeometry))
