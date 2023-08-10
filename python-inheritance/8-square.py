1d69c79d8f9a13e84c19f28ac1664e8bb779e96e
Hadiz <adefemihadizi2020@gmail.com>
2023-08-10 22:04:48 +0100

    PEP8 validated

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -5,16 +5,16 @@ Rectangle = __import__('7-rectangle').Rectangle
 
 class Square(Rectangle):
     """Represents an object as a Square"""
-    
+
     def __init__(self, size):
         """Initializes with the size of the square object"""
         super().integer_validator("size", size)
         self.__size = size
-        
+  
     def area(self):
         """calculates and returns the area of the square object"""
         return self.__size ** 2
-    
+
     def __str__(self):
         """represents the square object with its size"""
         return f"[Rectangle] {self.__size}/{self.__size}"
