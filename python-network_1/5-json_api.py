8cd4256cd02772e6018f6870aa35bc56ebdc5bb7
Hadizadefemi <adefemihadizi202@gmail.com>
2023-08-13 06:44:01 -0700

    task 4 done

---
+++ python-network_1/5-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user 
+with the letter as a parameter.
+"""
+
+
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    letter = sys.argv[1] if len(sys.argv[1]) > 1 else ""
+    params = {'q': letter}
+
+    response = requests.post(url, data=params)
+
+    try:
+        data = response.json()
+        if data:
+            print("[{}] {}".format(data['id'], data['name']))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
