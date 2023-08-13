094475ae61ae94c5b2170b72310ef984f4715f23
Hadizadefemi <adefemihadizi202@gmail.com>
2023-08-13 03:02:39 -0700

    task 1 done

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -8,4 +8,4 @@ if __name__ == '__main__':
     import requests
     import sys
     response = requests.get(sys.argv[1])
-    print(response.headers['X-Request-Id'])
+    print(response.headers.get('X-Request-Id'))
