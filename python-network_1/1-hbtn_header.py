b602b757109be07594b425ea1bce2902bef9a464
Hadizadefemi <adefemihadizi202@gmail.com>
2023-08-13 02:56:23 -0700

    task 1 done

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Takes in a URL, sends a request to the URL and displays
+the value of the variable X-Request-Id in the response header
+"""
+
+
+if __name__ == '__main__':
+    import requests
+    import sys
+    response = requests.get(sys.argv[1])
+    print(response.headers['X-Request-Id'])
