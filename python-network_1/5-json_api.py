a8056a6a6b49505c49afc211831e9306b87f3778
Hadizadefemi <adefemihadizi202@gmail.com>
2023-08-13 06:51:43 -0700

    corrected

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     import sys
     import requests
 
-    letter = sys.argv[1] if len(sys.argv[1]) > 1 else ""
+    letter = sys.argv[1] if len(sys.argv) > 1 else ""
     params = {'q': letter}
 
     url = "http://0.0.0.0:5000/search_user"
